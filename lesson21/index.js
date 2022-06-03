5dcd91571289e7a69c6ac44f6a80af9bbbcdac62
Richard <rmitsak3@gmail.com>
2022-06-04 02:34:17 +0300

    Add lesson 21 First JS lesson

---
+++ lesson21/index.js
@@ -0,0 +1,32 @@
+// let one = prompt('Vvedit cvoye 4islo, vid 1- do n:');
+// let two = 4 ;
+// console.log('BVidpovidb:', one * two);
+
+// let firstName = 'Oleg_Stepanov';
+// console.log(firstName);
+// let secondName = 'Oleksand-Petrobich';
+// console.log(secondName);
+// let thirdName = 'Stepan-Stuk';
+// console.log(thirdName);
+// let fourthName = 'Stas-Virna';
+// console.log(fourthName);
+
+/*
+let 1One = 'Text';
+let TWO = 'Text';
+let Три = 'Text';
+let Fo ur = 'Text';
+let five^^ = 'Text';
+*/
+
+// 1.camelcase
+// 2.kebabcase
+// 3.snakecase
+
+// let nameUser = prompt('Send your name:')
+// console.log(nameUser)
+
+// let years = prompt('Send your age')
+// let thisYears = 2022 ;
+// console.log('Your date of birth:',thisYears - years)
+
