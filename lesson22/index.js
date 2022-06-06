29e9f8a4fc715b937e7c447a9b45043b1ef85e01
Richard <rmitsak3@gmail.com>
2022-06-06 23:17:32 +0300

    lesson22

---
+++ lesson22/index.js
@@ -0,0 +1,34 @@
+// let num1 = 0.1;
+// let num2 = 0.2;
+// let result = num1 + num2;
+// console.log(result.toFixed(1))
+
+// let linestr = '1';
+// linestr = Number(linestr);
+// let linenum = 2;
+// let result = linestr + linenum;
+// console.log(result)
+
+// let gigabytes = prompt('Вкажіть число гігабайтів');
+// let file = 0.850;
+// let result = gigabytes / file;
+// console.log('Кількі файлів вагою в 850 Mb які помістяться на флешці буде:', Math.floor(result));
+
+// let userMoney = prompt('Вкажіть сумму:');
+// let thePriceOfChocolate = prompt('Вкажіть ціну шоколадки:');
+// let result = userMoney / thePriceOfChocolate;
+// console.log('Кількі шоколадок, яку ви можете придбати по вказаній ціні:', Math.floor(result));
+// let remainder = userMoney % thePriceOfChocolate;
+// console.log('Решта:', remainder);
+
+
+// Задача з банком
+
+// let amount = prompt('Введіть сумму вкладу:');
+// amount = Number(amount);
+// let PercentagePerAnnum1 = 100;
+// let PercentagePerAnnum2 = 5;
+// let result = amount / PercentagePerAnnum1 * PercentagePerAnnum2 + amount;
+// let onlyInterest = amount / PercentagePerAnnum1 * PercentagePerAnnum2;
+// let resultFinish = result / PercentagePerAnnum1 * PercentagePerAnnum2 + onlyInterest;
+// console.log('Сумма нарахованих відсотків за 2 місяця:', resultFinish);
