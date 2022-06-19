830f9ee396cf25139979b6d85b990463ec61b605
Richard <rmitsak3@gmail.com>
2022-06-19 16:20:02 +0300

    add main

---
+++ lesson23/index.js
@@ -0,0 +1,73 @@
+// let value = +prompt("Вкажіть ваш вік:");
+
+// if (value >= 0 && value <= 11) {
+//   console.log("Ви являєтесь дитиною");
+// } else if (value >= 12 && value <= 17) {
+//   console.log("Ви являєтесь підлітком");
+// } else if (value >= 18 && value <= 59) {
+//   console.log("Ви являєтесь дорослим");
+// } else if (value >= 60 && value <= 120) {
+//   console.log("Ви являєтесь старожителем");
+// } else if (value >= 120 && value <= 199) {
+//   console.log("Це неймовірно, дзвоніть в видавництво книги рекордів Гінеса!!");
+// } else if (
+//   value >=
+//     -999999999999999999999999999999999999999999999999999999999999999999999999999 &&
+//   value <= -1
+// ) {
+//   console.log("Непоганий жарт)");
+// } else if (
+//   value >= 200 &&
+//   value <=
+//     9999999999999999999999999999999999999999999999999999999999999999999999999
+// ) {
+//   console.log("Знову ваші жарти((");
+// }
+
+// let num = +prompt('Введіть число щоб отримати спецсимвол');
+
+// switch (num) {
+//    case 1:
+//       console.log('!');
+//       break
+//    case 2:
+//       console.log('@');
+//       break
+//    case 3:
+//       console.log('#');
+//       break
+//    case 4:
+//       console.log('$');
+//       break
+//    case 5:
+//       console.log('%');
+//       break
+//    case 6:
+//       console.log('^');
+//       break
+//    case 7:
+//       console.log('&');
+//       break
+//    case 8:
+//       console.log('*');
+//       break
+//    case 9:
+//       console.log('(');
+//       break
+//    case 0:
+//       console.log(')');
+//       break
+//    default:
+//       (num >= 10)
+//       console.log('Для цього числа немає спецсимволу')
+// }
+
+// Я відповідь погуглив, сам недійшов
+// let num = +prompt("Введіть перше число діапазону");
+// let num2 = +prompt("Введіть друге число діапазону");
+// let result = 0;
+// while (num < num2) {
+//    num++;
+//    result+=num;
+// }console.log(result)
+
